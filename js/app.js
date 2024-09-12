ba519c310107f1f7bffb8251529bd0039eff681b
flight505 <jesper_vang@me.com>
2024-09-12 03:54:57 +0200

    puss all

--- js/app.js
+++ js/app.js
@@ -1,326 +1,312 @@
-import * as THREE from "three";
-import { REVISION } from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
-import fragment from "./shader/fragment.glsl";
-import fragmentShaderVelocity from "./shader/fragmentShaderVelocity.glsl";
-import fragmentShaderPosition from "./shader/fragmentShaderPosition.glsl";
-import vertex from "./shader/vertexParticles.glsl";
-import { GPUComputationRenderer } from "three/examples/jsm/misc/GPUComputationRenderer.js";
-import GUI from "lil-gui";
-import t1 from '../ana.jpg';
-import t2 from '../2.png';
-import gsap from "gsap";
-import load from 'load-asset';
-import PoissonDiskSampling from 'poisson-disk-sampling';
+import * as THREE from "three"
+import { REVISION } from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js"
+import { GPUComputationRenderer } from "three/examples/jsm/misc/GPUComputationRenderer.js"
+import GUI from "lil-gui"
+import gsap from "gsap"
+import load from 'load-asset'
+import PoissonDiskSampling from 'poisson-disk-sampling'
+
+import fragment from "./shader/fragment.glsl"
+import fragmentShaderVelocity from "./shader/fragmentShaderVelocity.glsl"
+import fragmentShaderPosition from "./shader/fragmentShaderPosition.glsl"
+import vertex from "./shader/vertexParticles.glsl"
+
+import t1 from '../ana.jpg'
+import t2 from '../2.png'
+
 console.log(load)
 
+// Utility function to shuffle an array
 function shuffle(array) {
-  let currentIndex = array.length;
-
-  // While there remain elements to shuffle...
+  let currentIndex = array.length
   while (currentIndex != 0) {
-
-    // Pick a remaining element...
-    let randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+    let randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
   }
 }
 
+let COUNT = 128 // New value for more particles
+let TEXTURE_WIDTH = COUNT ** 2 // This will automatically update based on the COUNT value
 
-let COUNT = 128;
-let TEXTURE_WIDTH = COUNT ** 2;
-
+// Main Sketch class
 export default class Sketch {
   constructor(options) {
-    this.scene = new THREE.Scene();
+    // Initialize scene, renderer, and camera
+    this.scene = new THREE.Scene()
+    this.container = options.dom
+    this.width = this.container.offsetWidth
+    this.height = this.container.offsetHeight
+    this.renderer = new THREE.WebGLRenderer()
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    this.renderer.setSize(this.width, this.height)
+    this.renderer.setClearColor(0xEEEDF0, 1)
+    this.container.appendChild(this.renderer.domElement)
 
-    this.container = options.dom;
-    this.width = this.container.offsetWidth;
-    this.height = this.container.offsetHeight;
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    this.renderer.setSize(this.width, this.height);
-    this.renderer.setClearColor(0xeeeeee, 1);
+    // Set up camera and controls
+    this.camera = new THREE.PerspectiveCamera(30, this.width / this.height, 0.1, 10)
+    this.camera.position.set(0, 0, 2)
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.time = 0
 
-    this.container.appendChild(this.renderer.domElement);
+    // Set up loaders
+    const THREE_PATH = `https://unpkg.com/three@0.${REVISION}.x`
+    this.dracoLoader = new DRACOLoader(new THREE.LoadingManager()).setDecoderPath(`${THREE_PATH}/examples/jsm/libs/draco/gltf/`)
+    this.gltfLoader = new GLTFLoader()
+    this.gltfLoader.setDRACOLoader(this.dracoLoader)
 
-    this.camera = new THREE.PerspectiveCamera(
-      30,
-      this.width / this.height,
-      0.1,
-      10
-    );
+    this.isPlaying = true
 
-    // let frustumSize = 10;
-    // let aspect = this.width / this.height;
-    // this.camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, -1000, 1000 );
-    this.camera.position.set(0, 0, 2);
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.time = 0;
-
-    const THREE_PATH = `https://unpkg.com/three@0.${REVISION}.x`;
-    this.dracoLoader = new DRACOLoader(
-      new THREE.LoadingManager()
-    ).setDecoderPath(`${THREE_PATH}/examples/jsm/libs/draco/gltf/`);
-    this.gltfLoader = new GLTFLoader();
-    this.gltfLoader.setDRACOLoader(this.dracoLoader);
-
-    this.isPlaying = true;
-
+    // Initialize all components
     this.initAll()
-    
-    // this.setUpSettings();
   }
 
+  // Initialize all components
   async initAll() {
     this.points1 = await this.getPoints(t1)
     this.points2 = await this.getPoints(t2)
 
     this.initGPU()
-    this.addObjects();
-    this.resize();
-    this.render();
-    this.setupResize();
+    this.addObjects()
+    this.resize()
+    this.render()
+    this.setupResize()
   }
 
+  // Load image and generate points
   async getPoints(url) {
-    const image = await load(url);
-    let canvas = document.createElement("canvas");
-    let ctx = canvas.getContext("2d", { willReadFrequently: true });
-    canvas.width = COUNT;
-    canvas.height = COUNT;
-    ctx.drawImage(image, 0, 0, COUNT, COUNT);
-    let data = ctx.getImageData(0, 0, COUNT, COUNT).data;
-    // 2 dimensional array
-    let array = new Array(COUNT).fill().map(() => new Array(COUNT).fill(0));
+    const image = await load(url)
+    let canvas = document.createElement("canvas")
+    let ctx = canvas.getContext("2d", { willReadFrequently: true })
+    canvas.width = COUNT
+    canvas.height = COUNT
+    ctx.drawImage(image, 0, 0, COUNT, COUNT)
+    let data = ctx.getImageData(0, 0, COUNT, COUNT).data
+
+    // Create a 2D array to store color data
+    let array = Array.from({ length: COUNT }, () => Array(COUNT).fill(0))
     for (let i = 0; i < COUNT; i++) {
       for (let j = 0; j < COUNT; j++) {
-        let position = (i + j * COUNT) * 4;
-        let color = data[position] / 255;
-        array[i][j] = color;
+        let position = (i + j * COUNT) * 4
+        let color = data[position] / 255
+        array[i][j] = color
       }
     }
 
-
+    // Use Poisson Disk Sampling to generate points
     var pds = new PoissonDiskSampling({
       shape: [1, 1],
-      minDistance: 1/400,
-      maxDistance: 4/400,
-      tries: 20,
+      minDistance: 1 / 400,
+      maxDistance: 5 / 400,
+      tries: 30,
       distanceFunction: function (point) {
-          let indX = Math.floor(point[0]  * COUNT);
-          let indY = Math.floor(point[1]  * COUNT);
-          return array[indX][indY];
+        let indX = Math.floor(point[0] * COUNT)
+        let indY = Math.floor(point[1] * COUNT)
+        return array[indX][indY]
       },
       bias: 0
-  });
+    })
 
-  let points = pds.fill();
-  points.sort((a,b) => (Math.random()-0.5));
-  points = points.slice(0,TEXTURE_WIDTH);
+    let points = pds.fill()
+    points = points.filter(point => {
+      let indX = Math.floor(point[0] * COUNT)
+      let indY = Math.floor(point[1] * COUNT)
+      return array[indX][indY] < 0.9 // Filter out points in white areas
+    })
+    points.sort(() => Math.random() - 0.5)
+    points = points.slice(0, TEXTURE_WIDTH)
 
+    points = points.map((point) => {
+      let indX = Math.floor(point[0] * COUNT)
+      let indY = Math.floor(point[1] * COUNT)
+      let alpha = 1 - array[indX][indY] // Adjust alpha based on color intensity
+      return [point[0], point[1], alpha]
+    })
 
-  points = points.map((point) => {
-    let indX = Math.floor(point[0]  * COUNT);
-    let indY = Math.floor(point[1]  * COUNT);
-    return [point[0],point[1], array[indX][indY]];
-  })
-
-  
-
-  return points;
-
-
-    
-
+    return points
   }
 
+  // Setup GUI settings
   setUpSettings() {
-    this.settings = {
-      progress: 0,
-    };
-    this.gui = new GUI();
-    this.gui.add(this.settings, "progress", 0, 1, 0.01).onChange((val) => {});
+    this.settings = { progress: 0 }
+    this.gui = new GUI()
+    this.gui.add(this.settings, "progress", 0, 1, 0.01).onChange((val) => { })
   }
 
+  // Setup resize event listener
   setupResize() {
-    window.addEventListener("resize", this.resize.bind(this));
+    window.addEventListener("resize", this.resize.bind(this))
   }
 
+  // Handle window resize
   resize() {
-    this.width = this.container.offsetWidth;
-    this.height = this.container.offsetHeight;
-    this.renderer.setSize(this.width, this.height);
-    this.camera.aspect = this.width / this.height;
-    this.camera.updateProjectionMatrix();
+    this.width = this.container.offsetWidth
+    this.height = this.container.offsetHeight
+    this.renderer.setSize(this.width, this.height)
+    this.camera.aspect = this.width / this.height
+    this.camera.updateProjectionMatrix()
   }
 
+  // Add objects to the scene
   addObjects() {
     this.material = new THREE.ShaderMaterial({
-      extensions: {
-        derivatives: "#extension GL_OES_standard_derivatives : enable",
-      },
+      extensions: { derivatives: "#extension GL_OES_standard_derivatives : enable" },
       side: THREE.DoubleSide,
       uniforms: {
         time: { value: 0 },
         uPositions: { value: null },
         resolution: { value: new THREE.Vector4() },
       },
-      // wireframe: true,
       depthTest: false,
       depthWrite: false,
       transparent: true,
       vertexShader: vertex,
       fragmentShader: fragment,
-    });
+    })
 
-    // this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
-
-    this.geometry = new THREE.BufferGeometry();
-    let count = TEXTURE_WIDTH;
-    let positions = new Float32Array(count * 3);
-    let reference = new Float32Array(count * 2);
+    this.geometry = new THREE.BufferGeometry()
+    let count = TEXTURE_WIDTH
+    let positions = new Float32Array(count * 3)
+    let reference = new Float32Array(count * 2)
 
     for (let i = 0; i < count; i++) {
-      positions[i * 3] = 5 * (Math.random() - 0.5);
-      positions[i * 3 + 1] = 5 * (Math.random() - 0.5);
-      positions[i * 3 + 2] = 0;
-      reference[i * 2] = (i % COUNT)/COUNT;
-      reference[i * 2 + 1] = ~ ~ ( i / COUNT ) / COUNT;
+      positions[i * 3] = 5 * (Math.random() - 0.5)
+      positions[i * 3 + 1] = 5 * (Math.random() - 0.5)
+      positions[i * 3 + 2] = 0
+      reference[i * 2] = (i % COUNT) / COUNT
+      reference[i * 2 + 1] = ~~(i / COUNT) / COUNT
     }
 
-    this.geometry.setAttribute(
-      "position",
-      new THREE.BufferAttribute(positions, 3)
-    );
+    this.geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3))
+    this.geometry.setAttribute("reference", new THREE.BufferAttribute(reference, 2))
 
-    this.geometry.setAttribute(
-      "reference",
-      new THREE.BufferAttribute(reference, 2)
-    );
+    this.plane = new THREE.Points(this.geometry, this.material)
+    this.scene.add(this.plane)
 
-    // this.geometry = new THREE.PlaneGeometry(1,1,20,20);
-
-    this.plane = new THREE.Points(this.geometry, this.material);
-    this.scene.add(this.plane);
+    // Create line geometry
+    this.lineMaterial = new THREE.LineBasicMaterial({ color: 0x000000, transparent: true, opacity: 0. })
+    this.lineGeometry = new THREE.BufferGeometry()
+    this.linePositions = new Float32Array(count * 6) // Each line has 2 points, each with 3 coordinates
+    this.lineGeometry.setAttribute("position", new THREE.BufferAttribute(this.linePositions, 3))
+    this.lines = new THREE.LineSegments(this.lineGeometry, this.lineMaterial)
+    this.scene.add(this.lines)
   }
 
+  // Fill position texture with random values
   fillPositionTexture(texture) {
-    const theArray = texture.image.data;
-
+    const theArray = texture.image.data
     for (let k = 0, kl = theArray.length; k < kl; k += 4) {
-      theArray[k + 0] = 2*(Math.random() - 0.5);
-      theArray[k + 1] = 2*(Math.random() - 0.5);
-      theArray[k + 2] = 0;
-      theArray[k + 3] = 1;
+      theArray[k + 0] = 2 * (Math.random() - 0.5)
+      theArray[k + 1] = 2 * (Math.random() - 0.5)
+      theArray[k + 2] = 0
+      theArray[k + 3] = 1
     }
   }
 
+  // Fill velocity texture with random values
   fillVelocityTexture(texture) {
-    const theArray = texture.image.data;
+    const theArray = texture.image.data
     for (let k = 0, kl = theArray.length; k < kl; k += 4) {
-
-      theArray[k + 0] = 0.01*(Math.random() - 0.5);
-      theArray[k + 1] = 0.01*(Math.random() - 0.5);
-      theArray[k + 2] = 0;
-      theArray[k + 3] = 1;
+      theArray[k + 0] = 0.01 * (Math.random() - 0.5)
+      theArray[k + 1] = 0.01 * (Math.random() - 0.5)
+      theArray[k + 2] = 0
+      theArray[k + 3] = 1
     }
   }
 
-  fillPositionTextureFromPoints(texture,points){
-    const theArray = texture.image.data;
+  // Fill position texture from points
+  fillPositionTextureFromPoints(texture, points) {
+    const theArray = texture.image.data
     for (let k = 0, kl = theArray.length; k < kl; k += 4) {
-      let i = k / 4;
-      theArray[k + 0] = 2*(points[i][0] - 0.5);
-      theArray[k + 1] = -2*(points[i][1] - 0.5);
-      theArray[k + 2] = 0;
-      theArray[k + 3] = points[i][2];
+      let i = k / 4
+      theArray[k + 0] = 2 * (points[i][0] - 0.5)
+      theArray[k + 1] = -2 * (points[i][1] - 0.5)
+      theArray[k + 2] = 0
+      theArray[k + 3] = points[i][2]
     }
   }
 
+  // Initialize GPU computation
   initGPU() {
-    this.gpuCompute = new GPUComputationRenderer(COUNT, COUNT, this.renderer);
+    this.gpuCompute = new GPUComputationRenderer(COUNT, COUNT, this.renderer)
 
-    const dtPosition = this.gpuCompute.createTexture();
-    const dtPosition1 = this.gpuCompute.createTexture();
-    const dtVelocity = this.gpuCompute.createTexture();
-    this.fillPositionTextureFromPoints(dtPosition,this.points1);
-    this.fillPositionTextureFromPoints(dtPosition1,this.points2);
-    this.fillVelocityTexture(dtVelocity);
+    const dtPosition = this.gpuCompute.createTexture()
+    const dtPosition1 = this.gpuCompute.createTexture()
+    const dtVelocity = this.gpuCompute.createTexture()
+    this.fillPositionTextureFromPoints(dtPosition, this.points1)
+    this.fillPositionTextureFromPoints(dtPosition1, this.points2)
+    this.fillVelocityTexture(dtVelocity)
 
+    const target1 = this.gpuCompute.createTexture()
+    const target2 = this.gpuCompute.createTexture()
+    this.fillPositionTextureFromPoints(target1, this.points1)
+    this.fillPositionTextureFromPoints(target2, this.points2)
 
-    const target1 = this.gpuCompute.createTexture();
-    const target2 = this.gpuCompute.createTexture();
-    this.fillPositionTextureFromPoints(target1,this.points1);
-    this.fillPositionTextureFromPoints(target2,this.points2);
+    this.velocityVariable = this.gpuCompute.addVariable("textureVelocity", fragmentShaderVelocity, dtVelocity)
+    this.positionVariable = this.gpuCompute.addVariable("texturePosition", fragmentShaderPosition, dtPosition)
 
+    this.gpuCompute.setVariableDependencies(this.velocityVariable, [this.positionVariable, this.velocityVariable])
+    this.gpuCompute.setVariableDependencies(this.positionVariable, [this.positionVariable, this.velocityVariable])
 
-    this.velocityVariable = this.gpuCompute.addVariable(
-      "textureVelocity",
-      fragmentShaderVelocity,
-      dtVelocity
-    );
-    this.positionVariable = this.gpuCompute.addVariable(
-      "texturePosition",
-      fragmentShaderPosition,
-      dtPosition
-    );
+    this.positionUniforms = this.positionVariable.material.uniforms
+    this.velocityUniforms = this.velocityVariable.material.uniforms
 
-    this.gpuCompute.setVariableDependencies(this.velocityVariable, [
-      this.positionVariable,
-      this.velocityVariable,
-    ]);
-    this.gpuCompute.setVariableDependencies(this.positionVariable, [
-      this.positionVariable,
-      this.velocityVariable,
-    ]);
+    this.positionUniforms["time"] = { value: 0.0 }
+    this.velocityUniforms["time"] = { value: 1.0 }
+    this.velocityUniforms["uTarget"] = { value: target1 }
+    this.velocityVariable.wrapS = THREE.RepeatWrapping
+    this.velocityVariable.wrapT = THREE.RepeatWrapping
+    this.positionVariable.wrapS = THREE.RepeatWrapping
+    this.positionVariable.wrapT = THREE.RepeatWrapping
 
-    this.positionUniforms = this.positionVariable.material.uniforms;
-    this.velocityUniforms = this.velocityVariable.material.uniforms;
-
-    this.positionUniforms["time"] = { value: 0.0 };
-    this.velocityUniforms["time"] = { value: 1.0 };
-    this.velocityUniforms["uTarget"] = { value: target1 };
-    this.velocityVariable.wrapS = THREE.RepeatWrapping;
-    this.velocityVariable.wrapT = THREE.RepeatWrapping;
-    this.positionVariable.wrapS = THREE.RepeatWrapping;
-    this.positionVariable.wrapT = THREE.RepeatWrapping;
-
-    let modul = 0;
-    document.addEventListener("click", () => {  
-      if(modul == 0){
-        this.velocityUniforms["uTarget"] = { value: target2 };
-        modul = 1;
-      }else{
-        this.velocityUniforms["uTarget"] = { value: target1 };
-        modul = 0;
+    let modul = 0
+    document.addEventListener("click", () => {
+      if (modul == 0) {
+        this.velocityUniforms["uTarget"] = { value: target2 }
+        modul = 1
+      } else {
+        this.velocityUniforms["uTarget"] = { value: target1 }
+        modul = 0
       }
     })
 
-    this.gpuCompute.init();
+    this.gpuCompute.init()
   }
 
+  // Render loop
   render() {
-    if (!this.isPlaying) return;
-    this.time += 0.05;
-    this.gpuCompute.compute();
-    this.positionUniforms[ 'time' ].value = this.time;
-    this.velocityUniforms[ 'time' ].value = this.time;
+    if (!this.isPlaying) return
+    this.time += 0.05
+    this.gpuCompute.compute()
+    this.positionUniforms['time'].value = this.time
+    this.velocityUniforms['time'].value = this.time
 
-    this.material.uniforms.uPositions.value = this.gpuCompute.getCurrentRenderTarget( this.positionVariable ).texture;
+    this.material.uniforms.uPositions.value = this.gpuCompute.getCurrentRenderTarget(this.positionVariable).texture
+    this.material.uniforms.time.value = this.time
 
-    this.material.uniforms.time.value = this.time;
-    requestAnimationFrame(this.render.bind(this));
-    this.renderer.render(this.scene, this.camera);
+    // Update line positions
+    const positions = this.geometry.attributes.position.array
+    for (let i = 0; i < this.linePositions.length; i += 6) {
+      const idx1 = Math.floor(Math.random() * (positions.length / 3)) * 3
+      const idx2 = Math.floor(Math.random() * (positions.length / 3)) * 3
+      this.linePositions[i] = positions[idx1]
+      this.linePositions[i + 1] = positions[idx1 + 1]
+      this.linePositions[i + 2] = positions[idx1 + 2]
+      this.linePositions[i + 3] = positions[idx2]
+      this.linePositions[i + 4] = positions[idx2 + 1]
+      this.linePositions[i + 5] = positions[idx2 + 2]
+    }
+    this.lineGeometry.attributes.position.needsUpdate = true
+
+    requestAnimationFrame(this.render.bind(this))
+    this.renderer.render(this.scene, this.camera)
   }
 }
 
+// Initialize the Sketch
 new Sketch({
   dom: document.getElementById("container"),
-});
+})
--- js/shader/vertexParticles.glsl
+++ js/shader/vertexParticles.glsl
@@ -7,11 +7,11 @@ varying float vShade;
 float PI = 3.141592653589793238;
 void main() {
   vUv = uv;
-  vec3 pos = texture2D( uPositions, reference ).xyz;
+  vec3 pos = texture2D(uPositions, reference).xyz;
 
-  vShade = texture2D( uPositions, reference ).w;
+  vShade = texture2D(uPositions, reference).w;
 
-  vec4 mvPosition = modelViewMatrix * vec4( pos, 1. );
-  gl_PointSize = 45. * ( 1. / - mvPosition.z );
+  vec4 mvPosition = modelViewMatrix * vec4(pos, 1.);
+  gl_PointSize = 60. * (1. / -mvPosition.z); // Increased point size
   gl_Position = projectionMatrix * mvPosition;
 }
